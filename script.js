86b0eca5425646773735e68b0c4acde483a0cd38
Glitch (mixolydian-deserted-moon) <none>
2021-03-24 17:56:32 +0000

    🙌🎀 Checkpoint ./script.js:30058428/3178 ./index.html:30058428/206 ./style.css:30058428/2186

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 //Global Variables
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
-var progress = 0; 
+var progress = 0;
 var gamePlaying = false;
-// var tonePlaying = false;
-var volume = 0.5;  //must be between 0.0 and 1.0
+var tonePlaying = false;
+var volume = 0.5; //must be between 0.0 and 1.0
 
-
-function startGame(){
-    //initialize game variables
-    progress = 0;
-    gamePlaying = true;
-    // swap the Start and Stop buttons
-    document.getElementById("startBtn").classList.add("hidden");
-    document.getElementById("stopBtn").classList.remove("hidden");
+function startGame() {
+  //initialize game variables
+  progress = 0;
+  gamePlaying = true;
+  // swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
 }
 
-function endGame(){
-    progress = 0;
-    gamePlaying = false;
-    // swap the Start and Stop buttons
-    document.getElementById("stopBtn").classList.add("hidden");
-    document.getElementById("startBtn").classList.remove("hidden");
+function endGame() {
+  progress = 0;
+  gamePlaying = false;
+  // swap the Start and Stop buttons
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
 }
 
 // Sound Synthesis Functions
@@ -29,36 +28,34 @@ const freqMap = {
   2: 329.6,
   3: 392,
   4: 466.2
-}
+};
 
-function playTone(btn,len){ 
-  o.frequency.value = freqMap[btn]
-  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-  tonePlaying = true
-  setTimeout(function(){
-    stopTone()
-  },len)
+function playTone(btn, len) {
+  o.frequency.value = freqMap[btn];
+  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+  tonePlaying = true;
+  setTimeout(function() {
+    stopTone();
+  }, len);
 }
-function startTone(btn){
-  if(!tonePlaying){
-    o.frequency.value = freqMap[btn]
-    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
-    tonePlaying = true
+function startTone(btn) {
+  if (!tonePlaying) {
+    o.frequency.value = freqMap[btn];
+    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
+    tonePlaying = true;
   }
 }
-function stopTone(){
-    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
-    tonePlaying = false
+function stopTone() {
+  g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
+  tonePlaying = false;
 }
 
 //Page Initialization
 // Init Sound Synthesizer
-var context = new AudioContext()
-var o = context.createOscillator()
-var g = context.createGain()
-g.connect(context.destination)
-g.gain.setValueAtTime(0,context.currentTime)
-o.connect(g)
-o.start(0)
-
-
+var context = new AudioContext();
+var o = context.createOscillator();
+var g = context.createGain();
+g.connect(context.destination);
+g.gain.setValueAtTime(0, context.currentTime);
+o.connect(g);
+o.start(0);
