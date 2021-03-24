25a1056a8fb84d69bcefcadec3a7c0f1e98775db
Glitch (mixolydian-deserted-moon) <none>
2021-03-24 16:24:10 +0000

    🐽🗽 Checkpoint ./script.js:30058428/273

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
 var progress = 0; 
 var gamePlaying = false;
+
+function startGame(){
+    //initialize game variables
+    progress = 0;
+    gamePlaying = true;
+    // swap the Start and Stop buttons
+    document.getElementById("startBtn").classList.add("hidden");
+    document.getElementById("stopBtn").classList.remove("hidden");
+}
+
